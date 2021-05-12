bac18a4fe481d478f62380c7cf476c3720664f7f
Pawarit Thong-ek <arm.techhitz@gmail.com>
2021-05-12 18:59:08 +0700

    v0.3.0

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <br>NEW VERSION 0.3.0!!
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
